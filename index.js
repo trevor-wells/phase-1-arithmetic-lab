963a9f4ee78229ce562cacbecdc750ad2f2d81a0
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-03-17 22:24:15 -0600

    Final commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 1;
+const num2 = 62;
+const num3 = 4;
+const num4 = 5;
 
-// Write your code here
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() + 1);
+let mod = num3 % num4;
+let max = Math.max(1, 3, 20, 5);
